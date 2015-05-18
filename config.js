da4a9a2035d0366e98fcdd4a60d2fbc10eb20299
Daniel McGhan <dmcghan@gmail.com>
2015-05-18 18:10:35 -0400

    Experimenting with new functionality.

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: {
+        user: 'hr',
+        password: 'oracle',
+        connectString: 'localhost:1523/xe',
+        poolMax: 20,
+        poolMin: 2,
+        poolIncrement: 2,
+        poolTimeout: 4
+    }
+};
