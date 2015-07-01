e34041492cadb6c5b3203c30f42964213b596b4f
Dan McGhan <dmcghan@gmail.com>
2015-07-01 14:20:45 -0400

    Added tests for the PoolManager class

---
+++ test/testutil.js
@@ -0,0 +1,17 @@
+var util = require('util');
+
+module.exports.extend = function() {
+    return util._extend.apply(this, arguments);
+};
+
+module.exports.getConfigFromEnv = function() {
+    var config = {};
+
+    config.user = process.env.ORAWRAP_TEST_USER || 'hr';
+    config.password = process.env.ORAWRAP_TEST_PASSWORD || 'oracle';
+    config.connectString = process.env.ORAWRAP_TEST_CONNECT_STRING || 'localhost/xe';
+    config.poolMax = Number(process.env.ORAWRAP_TEST_POOL_MAX) || 20;
+    config.poolMin = Number(process.env.ORAWRAP_TEST_POOL_MIN) || 2;
+    config.poolIncrement = Number(process.env.ORAWRAP_TEST_POOL_INCREMENT) || 2;
+    config.poolTimeout = Number(process.env.ORAWRAP_TEST_POOL_TIMEOUT) || 10;
+};
