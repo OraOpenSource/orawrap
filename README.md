d57a4e7d1ab4125ae35a01882a86672b03172d28
Dan McGhan <dmcghan@gmail.com>
2015-07-01 20:12:49 -0400

    updated readme with more details

--- README.md
+++ README.md
@@ -2,13 +2,80 @@
 
 Orawrap is a wrapper module for the Oracle Database driver for Node.js ([node-oracledb](https://github.com/oracle/node-oracledb)). Some of the features include:
 
-* A simplified execute method that opens and closes connections automatically
-* An optional promise based API
+* A pool manager that provides pool storage and retrieval methods
+* A connection manager that provides a simplified execute method (can open and close connections automatically)
 * Support for centralizing SQL scripts to be executed at various timings
+
+###Example 1: One off script (no pool)
+
+
+###Example 2: Web server (pool)
+**server.js**
+```javascript
+var express = require('express');
+var app = express();
+var employeesRoutes = require(__dirname + '/routes/employees.js');
+var orawrap = require('orawrap');
+var dbConfig = {
+    user: 'hr',
+    password: 'welcome',
+    connectString: 'localhost/xe',
+    poolMax: 20,
+    poolMin: 2,
+    poolIncrement: 2,
+    poolTimeout: 10
+};
+
+//Add a handler for get requests on /api/employees
+app.get('/api/employees', employeesRoutes.get);
+
+//Use orawrap to create a connection pool prior to starting the web server 
+orawrap.createPool(dbConfig, function(err, pool) {//pool is provided but rarely needed as it's stored in orawrap for use later
+   if (err) throw err;
+   
+   //Start the web server now that the pool is ready to handle incoming requests
+   app.listen(3000, function() {
+       console.log('Web server listening on localhost: 3000');
+   });
+});
+```
+
+**/routes/employees.js**
+```javascript
+var orawrap = require('orawrap');
+
+//When orawrap is required in here, the pool is already available for use
+function get(req, res, next) {
+   //orawrap's execute will handle obtaining a connection from the pool and releasing it after execution
+   orawrap.execute(
+       'SELECT employee_id, ' +
+       '   first_name, ' +
+       '   last_name, ' +
+       '   phone_number, ' +
+       '   hire_date ' +
+       'FROM employees',
+       function(err, results) {
+          if (err) {
+             next(err);
+             return;
+          }
+          
+          res.send(results.rows);
+       }
+   );
+}
+
+module.exports.get = get;
+
+```
 
 ## Prerequisite
 
 Orawrap requires the [node-oracledb](https://github.com/oracle/node-oracledb) module to be available, either globally or locally. See the [installation documentation on node-oracledb] (https://github.com/oracle/node-oracledb/blob/master/INSTALL.md) for details on how to install the module in your environment.
+
+## Installation
+
+
 
 ## License
 
---
+++ test/connectionmanager.test.js
@@ -0,0 +1,263 @@
+var PoolManager = require(__dirname + '/../lib/poolmanager.js');
+var poolmanager;
+var OrawrapError = require(__dirname + '/../lib/orawraperror.js');
+var testUtil = require(__dirname + '/testutil.js');
+var assert = require('chai').assert;
+var config = {};
+var configEnv = testUtil.getConfigFromEnv();
+
+describe('poolmanager module:', function() {
+    beforeEach(function(){
+        poolmanager = new PoolManager();
+        config = testUtil.extend({}, configEnv);
+    });
+
+    describe('create pool', function() {
+        it('passes errors through the promise', function(done) {
+            //not passsing a config obj will cause an error in oracledb
+            poolmanager.createPool()
+                .then(function(pool) {
+                    done(new Error('created pool'));
+                })
+                .catch(function(err) {
+                    assert.instanceOf(err, Error);
+
+                    done();
+                });
+        });
+
+        it('passes error through the callback', function(done) {
+            //not passsing a config obj will cause an error in oracledb
+            poolmanager.createPool(undefined, function(err, pool) {
+                if (err) {
+                    assert.instanceOf(err, Error);
+                    done();
+                    return;
+                }
+
+                done(new Error('created pool'));
+            });
+        });
+
+        it('returns a promise which provides the pool', function(done) {
+            poolmanager.createPool(config)
+                .then(function(pool) {
+                    assert.equal(pool.connectionsInUse, 0);
+
+                    done();
+                })
+                .catch(function(err) {
+                    done(err);
+                });
+        });
+
+        it('executes a callback when supplied which provides the pool', function(done) {
+            poolmanager.createPool(config, function(err, pool) {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(pool.connectionsInUse, 0);
+
+                done();
+            });
+        });
+
+        it('creates a pool with the default name when poolName not supplied', function(done) {
+            poolmanager.createPool(config)
+                .then(function(pool) {
+                    assert.equal(pool.connectionsInUse, 0);
+                    assert.equal(pool.poolName, poolmanager._defaultPoolKey);
+
+                    done();
+                })
+                .catch(function(err) {
+                    done(err);
+                });
+        });
+
+        it('can create multiple connection pools', function(done) {
+            var pool1;
+            var pool2;
+
+            config.poolName = 'pool1';
+
+            poolmanager.createPool(config)
+                .then(function(p1) {
+                    pool1 = p1;
+
+                    config.poolName = 'pool2';
+
+                    return poolmanager.createPool(config);
+                })
+                .then(function(p2) {
+                    pool2 = p2;
+
+                    assert.equal(pool1.connectionsInUse, 0);
+                    assert.equal(pool2.connectionsInUse, 0);
+
+                    done();
+                })
+                .catch(function(err) {
+                    done(err);
+                });
+        });
+
+        it('ensures pool names are unique', function(done) {
+            config.poolName = 'pool1';
+
+            poolmanager.createPool(config)
+                .then(function() {
+                    return poolmanager.createPool(config);
+                })
+                .then(function() {
+                    done(new Error('Pool names were not unique'));
+                })
+                .catch(function(err) {
+                    assert.instanceOf(err, OrawrapError);
+                    assert.equal(err.message, poolmanager.DUP_POOL_NAME);
+
+                    done();
+                });
+        });
+
+        it('ensures default pool name not used explicitly', function(done) {
+            config.poolName = poolmanager._defaultPoolKey;
+
+            poolmanager.createPool(config)
+                .then(function() {
+                    done(new Error('Pool names were not unique'));
+                })
+                .catch(function(err) {
+                    assert.instanceOf(err, OrawrapError);
+                    assert.equal(err.message, poolmanager.DEFAULT_POOL_NAME_USED);
+
+                    done();
+                });
+        });
+
+        it('ensures pool names are used 1', function(done) {
+            poolmanager.createPool(config)
+                .then(function() {
+                    config.poolName = 'pool2';
+
+                    return poolmanager.createPool(config);
+                })
+                .then(function() {
+                    done(new Error('The first pool did not have a name'));
+                })
+                .catch(function(err) {
+                    assert.instanceOf(err, OrawrapError);
+                    assert.equal(err.message, poolmanager.FIRST_POOL_NOT_NAMED);
+
+                    done();
+                });
+        });
+
+        it('ensures pool names are used 2', function(done) {
+            config.poolName = 'pool1';
+
+            poolmanager.createPool(config)
+                .then(function() {
+                    delete config.poolName;
+
+                    return poolmanager.createPool(config);
+                })
+                .then(function() {
+                    done(new Error('The second pool did not have a name'));
+                })
+                .catch(function(err) {
+                    assert.instanceOf(err, OrawrapError);
+                    assert.equal(err.message, poolmanager.POOL_NOT_NAMED);
+
+                    done();
+                });
+        });
+    });
+
+    describe('terminate pool', function() {
+        it('works when passed the default pool', function(done) {
+            poolmanager.createPool(config, function(err, pool) {
+                if (err) {
+                    return done(err);
+                }
+
+                poolmanager.terminatePool(pool, function(err) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    done();
+                });
+            });
+        });
+
+        it('works when passed a non-default pool', function(done) {
+            config.poolName = 'not the default';
+
+            poolmanager.createPool(config, function(err, pool) {
+                if (err) {
+                    return done(err);
+                }
+
+                poolmanager.terminatePool(pool, function(err) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    done();
+                });
+            });
+        });
+
+        it('terminates the default pool when not passed a pool or pool name', function(done) {
+            poolmanager.createPool(config, function(err, pool) {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.isTrue(poolmanager.defaultPoolExists());
+
+                poolmanager.terminatePool(function(err) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    assert.isFalse(poolmanager.defaultPoolExists());
+
+                    done();
+                });
+            });
+        });
+
+        it('returns correct error if pool does not exist', function(done) {
+            var badPoolName = 'some pool that does not exist';
+
+            poolmanager.createPool(config, function(err, pool) {
+                if (err) {
+                    return done(err);
+                }
+
+                pool.poolName = badPoolName;
+
+                poolmanager.terminatePool(pool, function(err) {
+                    if (err) {
+                        var correctError;
+
+                        assert.instanceOf(err, OrawrapError);
+
+                        correctError = new OrawrapError(poolmanager.POOL_DOES_NOT_EXIST, {
+                            'POOL_NAME': badPoolName
+                        });
+
+                        assert.equal(correctError.message, err.message);
+
+                        return done();
+                    }
+
+                    done(new Error('Did not get the correct error'));
+                });
+            });
+        });
+    });
+});
